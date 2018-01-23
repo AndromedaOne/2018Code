96722366fb5fe476ec4279f91cd51de7ea53035f
Roz <Creepytoes@gmail.com>
2018-01-22 20:28:36 -0500

    Wrote a PID loop for the encoder, added a command to use PID loop, added a way to get encoder ticks from the encoders on the CANTalon. Has not been tested, will test ASAP.

---
+++ eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/commands/MoveUsingEncoderPID.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc4905.Galaktika.commands;
+
+import org.usfirst.frc4905.Galaktika.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class MoveUsingEncoderPID extends Command {
+	
+	private double m_setpoint = 0;
+
+    public MoveUsingEncoderPID(double setpoint) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	m_setpoint = setpoint;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.driveTrain.initializeEncoderPID();
+    	Robot.driveTrain.enableEncoderPID(m_setpoint);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.driveTrain.isDoneEncoderPID();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.move(0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
