2fbf709ef932cccc216d0bc06986764e0db0cfb3
Eris McCubbin <eriscora@gmail.com>
2018-01-22 19:48:23 -0500

    Added jaws subsystem

--- eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/commands/JawsOpenClose.java
+++ eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/commands/JawsOpenClose.java
@@ -2,6 +2,8 @@ package org.usfirst.frc4905.Galaktika.commands;
 
 import org.usfirst.frc4905.Galaktika.Robot;
 
+import Utilities.ButtonsEnumerated;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -9,31 +11,46 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class JawsOpenClose extends Command {
 
-    public JawsOpenClose() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.jaws);
-    }
+	public JawsOpenClose() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.jaws);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Joystick joystick = Robot.oi.getSubsystemController();
+		boolean isLeftBumperPressed = ButtonsEnumerated.getLeftButton(joystick);
+		boolean isRightBumperPressed = ButtonsEnumerated.getRightButton(joystick);
+		if(isRightBumperPressed && ! isLeftBumperPressed) {
+			//extend
+			Robot.jaws.contract();
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+		}else if(isLeftBumperPressed && ! isRightBumperPressed) {
+			//contract
+			Robot.jaws.extend();
+		}else {
+			//do nothing
+		}
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.jaws.stop();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
