d27252a1d295801f33069747351f2699faf8760b
henry0904 <zhangleyang0904@gmail.com>
2018-01-22 20:22:45 -0500

    added AutoCommands and fixed Autogroupcommands.

--- eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/groupcommands/AutoCommand.java
+++ eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/groupcommands/AutoCommand.java
@@ -1,8 +1,34 @@
 package org.usfirst.frc4905.Galaktika.groupcommands;
 
+import org.usfirst.frc4905.Galaktika.commands.AutoDriveForward;
+import org.usfirst.frc4905.Galaktika.commands.AutoTurnLeft;
+import org.usfirst.frc4905.Galaktika.commands.AutoTurnRight;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
+/**
+ *
+ */
 public abstract class AutoCommand extends CommandGroup {
+
+    public AutoCommand() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
 
 	protected static final double FORWARD_DISTANCE_TO_SCALE = 304.25;
 	protected static final double LATERAL_DISTANCE_TO_SCALE = 15.08;
@@ -10,17 +36,16 @@ public abstract class AutoCommand extends CommandGroup {
 	protected static final double LATERAL_DISTANCE_BETWEEN_PATHS = 0;
 
 	protected void turnRight() {
-		// TODO Auto-generated method stub
+		addSequential(new AutoTurnRight());
 		
 	}
 
 	protected void turnLeft() {
-		// TODO Auto-generated method stub
+		addSequential(new AutoTurnLeft());
 		
 	}
 
-	protected void driveForward(double forwardDistanceToScale) {
-		// TODO Auto-generated method stub
-		
+	protected void driveForward(double forwardDistanceInches) {
+		addSequential(new AutoDriveForward(forwardDistanceInches));
 	}
 }
