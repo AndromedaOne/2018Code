cda69201f011c93e33ef23f7a28665174b7f3142
MrPixils <zacharymalstrom@gmail.com>
2018-01-25 20:34:18 -0500

    Adds a slowmo to the LEFT TRIGGER and sets the mod to 0.6

--- eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/commands/TeleOpDrive.java
+++ eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/commands/TeleOpDrive.java
@@ -64,32 +64,33 @@ public class TeleOpDrive extends Command {
 
 
 
-// Called repeatedly when this Command is scheduled to run
-@Override
-protected void execute() {
-	Joystick drivecontroller = Robot.oi.getDriveController();
-	double forwardBackwardStickValue = EnumeratedRawAxis.getLeftStickVertical(drivecontroller);
-	double rotateStickValue = EnumeratedRawAxis.getRightStickHorizontal(drivecontroller);
-	if(forwardBackwardStickValue < kDeadzone && forwardBackwardStickValue > -kDeadzone) {
-		forwardBackwardStickValue = 0;
-	}
-
-	if(rotateStickValue < kDeadzone && rotateStickValue > -kDeadzone) {
-		rotateStickValue = 0;
-	}
-
-	//24=about half a second
-	if(m_slowmodedelaycounter > 24 && Utilities.ButtonsEnumerated.getLeftButton(drivecontroller)) {
-		m_slowmodedelaycounter = 0;
-		if (!slowMoEnabled) {
-			mod = 0.3;
-			slowMoEnabled = true;
-			System.out.println("Slow Mode IS enabled!");
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Joystick drivecontroller = Robot.oi.getDriveController();
+		double forwardBackwardStickValue = EnumeratedRawAxis.getLeftStickVertical(drivecontroller);
+		double rotateStickValue = EnumeratedRawAxis.getRightStickHorizontal(drivecontroller);
+		if(forwardBackwardStickValue < kDeadzone && forwardBackwardStickValue > -kDeadzone) {
+			forwardBackwardStickValue = 0;
 		}
-		else {
-			mod = 1;
-			slowMoEnabled = false;
-			System.out.println("SLOWMODE HAS ENDED!");
+
+		if(rotateStickValue < kDeadzone && rotateStickValue > -kDeadzone) {
+			rotateStickValue = 0;
+		}
+
+		//24=about half a second
+		if(m_slowmodedelaycounter > 24 && Utilities.ButtonsEnumerated.getLeftButton(drivecontroller)) {
+			m_slowmodedelaycounter = 0;
+			if (!slowMoEnabled) {
+				mod = 0.6;
+				slowMoEnabled = true;
+				System.out.println("Slow Mode IS enabled!");
+			}
+			else {
+				mod = 1;
+				slowMoEnabled = false;
+				System.out.println("SLOWMODE HAS ENDED!");
+			}
 		}
 		m_slowmodedelaycounter++;
 
@@ -118,24 +119,24 @@ protected void execute() {
 				new TracePair("correctionMode", (double)correctionMode));
 	}
 
-}
 
-// Make this return true when this Command no longer needs to run execute()
-@Override
-protected boolean isFinished() {
-	return false;
-}
 
-// Called once after isFinished returns true
-@Override
-protected void end() {
-	Robot.driveTrain.stop();
-}
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 
-// Called when another command which requires one or more of the same
-// subsystems is scheduled to run
-@Override
-protected void interrupted() {
-	end();
-}
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		Robot.driveTrain.stop();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		end();
+	}
 }
