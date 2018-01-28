e3dfd6c71021c2415fc0f6050353e1d23ca42b00
Roz <Creepytoes@gmail.com>
2018-01-28 14:48:28 -0500

    Added methods for controlling elevator manually

---
+++ eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/commands/ElevatorManualControl.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc4905.Galaktika.commands;
+
+import org.usfirst.frc4905.Galaktika.Robot;
+
+import Utilities.EnumeratedRawAxis;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ElevatorManualControl extends Command {
+
+    public ElevatorManualControl() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.elevator);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Joystick subsystemController = Robot.oi.getSubsystemController();
+    	double forwardBackwardStickValue = EnumeratedRawAxis.getLeftStickVertical(subsystemController);
+    	if((Robot.elevator.getTopLimitSwitch() == true) && (forwardBackwardStickValue > 0)) {
+    		Robot.elevator.moveElevator(0);
+    	}
+    	else if ((Robot.elevator.getBottomLimitSwitch() == true) && (forwardBackwardStickValue < 0)) {
+    		Robot.elevator.moveElevator(0);
+    	}
+    	else {
+    		Robot.elevator.moveElevator(forwardBackwardStickValue);
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.elevator.moveElevator(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	Robot.elevator.moveElevator(0);
+    }
+}
