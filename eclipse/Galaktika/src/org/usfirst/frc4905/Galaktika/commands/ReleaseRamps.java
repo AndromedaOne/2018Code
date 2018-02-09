33d7e7cbc8f084455d069c7ff00d425251da0967
Unknown <will@ayerdoyles.org>
2018-02-08 19:43:10 -0500

    Code to be tested for ramp safeties.

--- eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/commands/ReleaseRamps.java
+++ eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/commands/ReleaseRamps.java
@@ -12,41 +12,61 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class ReleaseRamps extends Command {
 
+
+
 	Joystick driveController;
-    public ReleaseRamps() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	public ReleaseRamps() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.ramps);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	driveController = Robot.oi.getDriveController();
-    }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	
-    	boolean isLeftPOVPressed = POVDirectionNames.getPOVWest(driveController);
-    	boolean isRightPOVPressed = POVDirectionNames.getPOVEast(driveController);
-    	if (isLeftPOVPressed) {
-    		Robot.ramps.releaseLeftRamp();
-    	}
-    	if (isRightPOVPressed) {
-    		Robot.ramps.releaseRightRamp();
-    	}
-    }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		driveController = Robot.oi.getDriveController();
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+		boolean isLeftPOVPressed = POVDirectionNames.getPOVWest(driveController);
+		boolean isRightPOVPressed = POVDirectionNames.getPOVEast(driveController);
+		boolean safetiesEnabled = Robot.ramps.returnSafetyStatus();
+		double timeRemaining = Robot.ramps.getTimeRemainingInMatchPeriod();
+
+
+		if(safetiesEnabled){
+			if (isLeftPOVPressed && timeRemaining < 30) {
+				Robot.ramps.releaseLeftRamp();
+			}
+			if (isRightPOVPressed && timeRemaining < 30) {
+				Robot.ramps.releaseRightRamp();
+			}
+		}
+		else{
+			if (isLeftPOVPressed) {
+				Robot.ramps.releaseLeftRamp();
+			}
+			if (isRightPOVPressed) {
+				Robot.ramps.releaseRightRamp();
+			}
+		}
+
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
