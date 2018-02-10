9256b765cdd5a7ce1742b98019b0d2577418daed
BestOwen <owen@macfisherman.com>
2018-02-10 15:28:57 -0500

    Moved LEDs from PWM to DigitalOutput

--- eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/subsystems/LEDs.java
+++ eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/subsystems/LEDs.java
@@ -2,59 +2,59 @@ package org.usfirst.frc4905.Galaktika.subsystems;
 
 import org.usfirst.frc4905.Galaktika.RobotMap;
 
-import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class LEDs extends Subsystem {
 
 
-	private static PWM red = RobotMap.redVal;
-	private static PWM blue = RobotMap.blueVal;
-	private static PWM green = RobotMap.greenVal;
+	private static DigitalOutput red = RobotMap.redVal;
+	private static DigitalOutput blue = RobotMap.blueVal;
+	private static DigitalOutput green = RobotMap.greenVal;
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 	}
 	public void setPurple()  {
 		clearColor();
-		red.setRaw(255);
-		blue.setRaw(255);
+		red.updateDutyCycle(1);
+		blue.updateDutyCycle(1);
 	}
 
-	public void setRed()  {		 
-		clearColor();		
-		red.setRaw(255);		
-	}		
-	public void setBlue()  {		
-		clearColor();		
-		blue.setRaw(255);		
-	}		
-	public void setGreen()  {		
-		clearColor();		
-		red.setRaw(255);		
-		green.setRaw(255);		
+	public void setRed()  {
+		clearColor();
+		red.updateDutyCycle(1);
+	}
+	public void setBlue()  {
+		clearColor();
+		blue.updateDutyCycle(1);
+	}
+	public void setGreen()  {
+		clearColor();
+		red.updateDutyCycle(1);
+		green.updateDutyCycle(1);
 	}
 	public void setColor(int Red, int Green, int Blue) {
 		clearColor();
-		red.setRaw(Red);
+/*		red.setRaw(Red);
 		blue.setRaw(Blue);
-		green.setRaw(Green);
+		green.setRaw(Green); */
 	}
 
 	public void clearColor() {
-		red.setRaw(0);
-		blue.setRaw(0);
-		green.setRaw(0);
+		red.updateDutyCycle(0);
+		blue.updateDutyCycle(0);
+		green.updateDutyCycle(0);
 	}
 
 	public void setStatusGood() {
 		clearColor();
-		green.setRaw(255);
+		green.updateDutyCycle(1);
 	}
 
 	public void setStatusBad() {
 		clearColor();
-		red.setRaw(255);
+		red.updateDutyCycle(1);
 	}
 
 
