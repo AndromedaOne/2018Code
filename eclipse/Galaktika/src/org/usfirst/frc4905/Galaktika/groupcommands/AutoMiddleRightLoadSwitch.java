db41d71a6112e3c55981d3759acbd8669099b9ac
Unknown <will@ayerdoyles.org>
2018-02-16 18:28:31 -0500

    Auto stash before merge of "WeekZeroCode" and "EvolvedElevatorTestingBranch"

--- eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/groupcommands/AutoMiddleRightLoadSwitch.java
+++ eclipse/Galaktika/src/org/usfirst/frc4905/Galaktika/groupcommands/AutoMiddleRightLoadSwitch.java
@@ -25,7 +25,7 @@ public class AutoMiddleRightLoadSwitch extends AutoCommand {
             turnRight();
         }
         driveForwardToWall();
-        loadPowerCubeOntoSwitch();
+       
     }
 
 }
