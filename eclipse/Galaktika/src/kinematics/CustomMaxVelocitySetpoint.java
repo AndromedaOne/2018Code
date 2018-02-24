bdd87d2c874eba5060bd4307f6e42318ff0dfeba
Sean Doyle <sean@ayerdoyles.org>
2018-02-24 15:48:59 -0500

    WIP

--- eclipse/Galaktika/src/kinematics/CustomMaxVelocitySetpoint.java
+++ eclipse/Galaktika/src/kinematics/CustomMaxVelocitySetpoint.java
@@ -1,15 +1,31 @@
 package kinematics;
 
+/**
+ * This class is used in the Unit Cases class to define a setpoint that has a
+ * max velocity different from the max velocity specified by the create
+ * trajectory method.
+ * 
+ * @author seandoyle
+ *
+ */
 public class CustomMaxVelocitySetpoint {
 	private double m_setpoint;
 	private double m_customMaxVelocity = 0.0;
+
 	public CustomMaxVelocitySetpoint(double setpoint, double customMaxVelocity) {
 		m_setpoint = setpoint;
 		m_customMaxVelocity = customMaxVelocity;
 	}
+
 	public CustomMaxVelocitySetpoint(double setpoint) {
 		m_setpoint = setpoint;
 	}
-	public double getSetpoint() {return m_setpoint;}
-	public double getCustomMaxVelocity() {return m_customMaxVelocity;}
+
+	public double getSetpoint() {
+		return m_setpoint;
+	}
+
+	public double getCustomMaxVelocity() {
+		return m_customMaxVelocity;
+	}
 }
--- eclipse/Galaktika/src/kinematics/GettingOfTrajectoryPoint.java
+++ eclipse/Galaktika/src/kinematics/GettingOfTrajectoryPoint.java
@@ -1,18 +1,25 @@
 package kinematics;
 
-
+/**
+ * This class hold the methods used to get trajectory points for a specific time
+ * on a path.
+ * 
+ * @author seandoyle
+ *
+ */
 
 public class GettingOfTrajectoryPoint {
+
 	/**
 	 * This method takes a delta time from the beginning of a trajectory path and
 	 * returns the point where that time is
 	 */
-
 	public static TrajectoryPoint getTrajectoryPoint(Path Key, double deltaTimeFromStartOfSetpointVector) {
 		return getTrajectoryPoint(Key, deltaTimeFromStartOfSetpointVector, false);
 	}
 
-	public static TrajectoryPoint getTrajectoryPoint(Path Key, double deltaTimeFromStartOfSetpointVector, Boolean debugMode) {
+	public static TrajectoryPoint getTrajectoryPoint(Path Key, double deltaTimeFromStartOfSetpointVector,
+			Boolean debugMode) {
 		double velocity = 0.0;
 		double deltaPosition = Double.NaN;
 		double time = deltaTimeFromStartOfSetpointVector;
@@ -21,9 +28,17 @@ public class GettingOfTrajectoryPoint {
 		double previousEndDeltaTimes = 0.0;
 		double previousPositions = 0.0;
 		int setpointIndex = -1;
+		// Go through as many setpoints in the setpoint vector needed in order to find
+		// the setpoint during which point in time being looked for falls.
 		for (int i = 0; i < Key.setpointVector.size(); i++) {
 			setpoint = Key.setpointVector.get(i);
 			setpointIndex++;
+			// If the time being asked for is before the end time of the setpoint that means
+			// you have found the setpoint in which the trajectory point falls in so you can
+			// break.
+			// Else see if there is a setpoint beyond the current setpoint... If there is
+			// not break, if there is accumulate the previous positions and end Delta times
+			// for later use.
 			if (setpoint.endDeltaTime >= deltaTimeFromStartOfSetpointVector) {
 				break;
 			} else {
@@ -40,16 +55,43 @@ public class GettingOfTrajectoryPoint {
 
 		double deltaTimeFromSetpoint = deltaTimeFromStartOfSetpointVector - previousEndDeltaTimes;
 
+		// This logic goes through and calculates the velocity of the point in time
+		// being looked at as well as defines what BeforeWhatTime should be equal to
+
+		// If the delta time is 0 that means that velocity and delta position from the
+		// setpoint must be 0
+		// THIS IS WRONG!!!! THE VELOCITY IS NOT NECCESARILY 0!!!
 		if (deltaTimeFromSetpoint <= 0) {
 			velocity = 0.0;
 			deltaPosition = 0.0;
 
-		} else if (deltaTimeFromSetpoint < setpoint.firstStartAccelerationCruisingDeltaTime) {
+		}
+		// Else if the delta time is before the firstStartAccelerationCruisingDeltaTime
+		// that means that velocity can be calculated by using the kinematic equation
+		// distance = vi*t + 1/2*a*t^2. In this instance distance is actually equal to
+		// velocity covered, vi is equal to the initial acceleration, a is equal to the
+		// jerk, and t is equal to deltaTimeFromSetpoint.
+		else if (deltaTimeFromSetpoint < setpoint.firstStartAccelerationCruisingDeltaTime) {
 			beforeWhatTime = BeforeWhatTime.beforeFirstStartAccelerationCruisingDeltaTime;
 
 			velocity = 0.5 * Key.maxJerk * Math.pow(deltaTimeFromSetpoint, 2);
 
-		} else if (deltaTimeFromSetpoint < setpoint.firstEndAccelerationCruisingDeltaTime) {
+		}
+		// Else if the delta time is after firstStartAccelerationCruisingDeltaTime but
+		// before firstEndAccelerationCruisingDeltaTime calculate velocity by finding
+		// the velocity covered in between 0 and firstStartAccelerationCruisingDeltaTime
+		// and add that to the velocity covered in between the current delta time and
+		// firstStartAccelerationCruisingDeltaTime. Calculate the velocity covered in
+		// between 0 and firstStartAccelerationCruisingDeltaTime by using the kinematic
+		// equation distance = vi*t + 1/2*a*t^2 where distance is velocity covered, vi
+		// is the initial acceleration, a is the jerk
+		// and t is the firstStartAccelerationCruisingDeltaTime. Calculate the velocity
+		// covered in between firstStartAccelerationCruisingDeltaTime and
+		// deltaTimeFromSetpoint by using the kinematic equation vf = vi + a*t where vf
+		// is the final velocity, a is acceleration and t is time. You can assume vi is
+		// 0 because vi is just the initial velocity covered and that is added in when
+		// you add together the two velocities covered.
+		else if (deltaTimeFromSetpoint < setpoint.firstEndAccelerationCruisingDeltaTime) {
 
 			beforeWhatTime = BeforeWhatTime.beforeFirstEndAccelerationCruisingDeltaTime;
 
@@ -62,7 +104,22 @@ public class GettingOfTrajectoryPoint {
 
 			velocity = jerkVelocityCovered + velocityCoveredWhileAtMaxAcceleration;
 
-		} else if (deltaTimeFromSetpoint < setpoint.startVelocityCruisingDeltaTime) {
+		}
+		// Else if the delta time is before the startVelocityCruisingDeltaTime but after
+		// firstEndAccelerationCruisingDeltaTime calculate the velocity by getting the
+		// velocity covered between 0 and firstEndAccelerationCruisingDeltaTime and
+		// adding that to the velocity covered in between
+		// firstEndAccelerationCruisingDeltaTime and deltaTime. Calculate the velocity
+		// covered between 0 and firstEndAccelerationCruisingDeltaTime in the same way
+		// velocity was calculated in the above logic except when multiplying time by
+		// acceleration instead of using delta time use the time in between
+		// firstStartAccelerationCruisingDeltaTime and
+		// firstEndAccelerationCruisingDeltaTime. Calculate the velocity covered between
+		// deltaTime and firstEndAccelerationCruisingDeltaTime by using the kinematic
+		// equation vf^2 = vi^2 + 2*a*d where vf is the max acceleration, vi is the
+		// current acceleration, a is jerk and d is velocity covered. Finally take these
+		// velocities and add them together to ge the current velocity.
+		else if (deltaTimeFromSetpoint < setpoint.startVelocityCruisingDeltaTime) {
 			beforeWhatTime = BeforeWhatTime.beforeStartVelocityCruisingDeltaTime;
 
 			double firstStartJerkVelocityCovered = setpoint.maxAcceleration / 2
@@ -81,12 +138,23 @@ public class GettingOfTrajectoryPoint {
 			velocity = firstStartJerkVelocityCovered + velocityCoveredWhileCruisingAtMaxAcceleration
 					+ firstEndJerkVelocityCovered;
 
-		} else if (deltaTimeFromSetpoint < setpoint.endVelocityCruisingDeltaTime) {
+		}
+		// Else if the deltaTime is before the endVelocityCruisingDeltaTime but after
+		// startVelocityCruisingDeltaTime velocity must be max velocity because this is
+		// the time when velocity is cruising at max velocity.
+		else if (deltaTimeFromSetpoint < setpoint.endVelocityCruisingDeltaTime) {
 			beforeWhatTime = BeforeWhatTime.beforeEndVelocityCruisingDeltaTime;
 
 			velocity = setpoint.maxVelocity;
 
-		} else if (deltaTimeFromSetpoint < setpoint.secondStartAccelerationCruisingDeltaTime) {
+		}
+		// Else if the deltaTime is before secondStartAccelerationCruisingDeltaTime
+		// but after endVelocityCruisingDeltaTime find velocity by subtracting the
+		// velocity covered between endVelocityCruisingDeltaTime and deltaTime from max
+		// velocity. To calculate the velocity covered between
+		// endVelocityCruisingDeltaTime and deltaTime refer to the comments above to
+		// calculate velocity covered in between points
+		else if (deltaTimeFromSetpoint < setpoint.secondStartAccelerationCruisingDeltaTime) {
 			beforeWhatTime = BeforeWhatTime.beforeSecondStartAccelerationCruisingDeltaTime;
 
 			double secondEndJerkVelocity = setpoint.maxAcceleration / 2
@@ -101,7 +169,13 @@ public class GettingOfTrajectoryPoint {
 			velocity = secondEndJerkVelocity + velocityCoveredWhileCruisingAtMaxAcceleration
 					+ secondStartJerkVelocityCovered;
 
-		} else if (deltaTimeFromSetpoint < setpoint.secondEndAccelerationCruisingDeltaTime) {
+		}
+		// Else if deltaTime is before secondEndAccelerationCruisingDeltaTime but after
+		// secondStartAccelerationCruisingDeltaTime calculate velocity by subtracting
+		// the velocity covered between deltaTime and endVelocityCruisingDeltaTime from
+		// the max velocity. To calculate this refer to the comments above to
+		// calculate velocity covered in between points
+		else if (deltaTimeFromSetpoint < setpoint.secondEndAccelerationCruisingDeltaTime) {
 			beforeWhatTime = BeforeWhatTime.beforeSecondEndAccelerationCruisingDeltaTime;
 
 			double secondStartJerkVelocity = Key.maxAcceleration / 2
@@ -110,7 +184,9 @@ public class GettingOfTrajectoryPoint {
 					* (setpoint.secondEndAccelerationCruisingDeltaTime - deltaTimeFromSetpoint);
 			velocity = secondStartJerkVelocity + velocityCoveredWhileAtMaxAcceleration;
 
-		} else if (deltaTimeFromSetpoint <= setpoint.endDeltaTime + previousEndDeltaTimes) {
+		} 
+		// Else if the deltaTime is before 
+		else if (deltaTimeFromSetpoint <= setpoint.endDeltaTime) {
 			beforeWhatTime = BeforeWhatTime.beforeEndDeltaTime;
 
 			velocity = 0.5 * Math.pow((setpoint.endDeltaTime - deltaTimeFromSetpoint), 2) * Key.maxJerk;
@@ -144,7 +220,7 @@ public class GettingOfTrajectoryPoint {
 
 		return point;
 	}
-	
+
 	private static double getPosition(Point setpoint, double deltaTime, double maxJerk, double maxAcceleration,
 			BeforeWhatTime beforeWhatTime, Path Key) {
 		double position = 0.0;
